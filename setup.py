559de2092bc3a46f73b2fe6e9fd8c93e6cd6a9aa
Toshimitsu Kimura <lovesyao@gmail.com>
2022-11-28 12:00:48 +0900

    version bump to 0.0.6.1

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ install_requires = [
 
 setup(
     name = "gtk_stable_diffusion",
-    version = "0.0.6",
+    version = "0.0.6.1",
     author = "Toshimitsu Kimura",
     author_email = "lovesyao@gmail.com",
     description = ("A simple GTK UI for Stable Diffusion."),
