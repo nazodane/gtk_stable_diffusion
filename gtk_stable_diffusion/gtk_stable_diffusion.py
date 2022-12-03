378e04cd1016985b410d98fddd2af681a7ff4ebc
Toshimitsu Kimura <lovesyao@gmail.com>
2022-12-04 02:23:31 +0900

    use nested menu in auto-candicate system

--- gtk_stable_diffusion/gtk_stable_diffusion.py
+++ gtk_stable_diffusion/gtk_stable_diffusion.py
@@ -755,28 +755,41 @@ last_neg_prompt = """ + '"""' + (conf["last_neg_prompt"] if "last_neg_prompt" in
 
             import regex as re
             target_words = re.sub(r"""["'()+-_/\\|]""", " ", target_words)
-            print(target_words)
-            print(target_words.split(" "))
+#            print(target_words)
+#            print(target_words.split(" "))
 
-            candidates = []
-            if self._id == "prompt":
-                candidates =  [[x.lemma_names() for x in  wordnet.synsets(w)] for w in target_words.split(" ")]
-            elif self._id == "neg_prompt":
-                candidates = [[[[k.name() for k in i.antonyms()] for i in x.lemmas()] for x in  wordnet.synsets(w)] for w in target_words.split(" ")]
+            for prompt in target_words.split(" "):
+                synsets = wordnet.synsets(prompt)
+                if not synsets:
+                    continue
+                p_menu = Gtk.MenuItem.new_with_label(prompt)
+                p_menu._tv = self
+                p_menu_child = Gtk.Menu()
+                p_menu.set_submenu(p_menu_child)
 
-            def flatten_and_uniq_str(arr):
-                return {i for t in [[i] if isinstance(i, str) else flatten_and_uniq_str(i) for i in arr] for i in t}
+                candidates = []
+                if self._id == "prompt":
+                    candidates =  [x.lemma_names() for x in synsets]
+                elif self._id == "neg_prompt":
+                    candidates = [[[k.name() for k in i.antonyms()] for i in x.lemmas()] for x in synsets]
 
-            def on_prompt_add(self):
-                self._tv.get_buffer().insert_at_cursor(self.get_label())
+                def flatten_and_uniq_str(arr):
+                    return {i for t in [[i] if isinstance(i, str) else flatten_and_uniq_str(i) for i in arr] for i in t}
 
-            candidates = flatten_and_uniq_str(candidates)
-            for candidate in candidates:
-                cd_menu = Gtk.MenuItem.new_with_label(candidate)
-                cd_menu._tv = self
-                menu.append(cd_menu)
-                cd_menu.connect("activate", on_prompt_add)
-                cd_menu.show()
+                def on_prompt_add(self):
+                    self._tv.get_buffer().insert_at_cursor(self.get_label())
+
+                candidates = flatten_and_uniq_str(candidates)
+                for candidate in candidates:
+                    cd_menu = Gtk.MenuItem.new_with_label(candidate)
+                    cd_menu._tv = self
+                    p_menu_child.append(cd_menu)
+                    cd_menu.connect("activate", on_prompt_add)
+                    cd_menu.show()
+
+                menu.append(p_menu)
+                p_menu.show()
+
             menu.popup(None, None, None, None, 0, Gtk.get_current_event_time())
 
         def new_prompt_textview(self, lm, _id):
