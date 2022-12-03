53790482330079e610a5f02b943203e04c73adc1
Toshimitsu Kimura <lovesyao@gmail.com>
2022-12-04 02:25:51 +0900

    version bump to 0.0.8.1

--- gtk_stable_diffusion/__init__.py
+++ gtk_stable_diffusion/__init__.py
@@ -1,2 +1,2 @@
-__version__ = "0.0.8"
+__version__ = "0.0.8.1"
 from .gtk_stable_diffusion import main
--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ install_requires = [
 
 setup(
     name = "gtk_stable_diffusion",
-    version = "0.0.8",
+    version = "0.0.8.1",
     author = "Toshimitsu Kimura",
     author_email = "lovesyao@gmail.com",
     description = ("A simple GTK UI for Stable Diffusion."),
